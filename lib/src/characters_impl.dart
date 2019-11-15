f1e975a9fd098ae74cae3cac0f65598b8dbfa8dc
Lasse R.H. Nielsen <lrn@google.com>
2019-11-15 13:37:20 +0100

    Remove unnecessary import.

--- lib/src/characters_impl.dart
+++ lib/src/characters_impl.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import "dart:collection";// show ListBase;
-
 import 'package:characters/src/grapheme_clusters/table.dart';
 
 import "characters.dart";
